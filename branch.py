d5f9f9483ca2a33a87c5f7a311151d37d23fe585
Akihiro Matsukawa <akihiro.matsukawa@gmail.com>
2020-01-12 23:13:06 -0500

    Aliases and branch

--- branch.py
+++ branch.py
@@ -1,12 +1,11 @@
 import os
-import sys
 
 import shell
 import utils
 
 
 def git_branch():
-    os.makedirs(os.path.dirname( utils.CACHE_PATH), exist_ok=True)
+    os.makedirs(os.path.dirname(utils.CACHE_PATH), exist_ok=True)
     stdout, *_ = shell.run(["git", "branch"])
     branches = stdout.split("\n")[:-1]  # ends in a "\n"
     with open(utils.CACHE_PATH, "w") as cache:
